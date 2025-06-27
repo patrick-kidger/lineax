5124aaedab9469331fc94c620fd86457602b300b
Jonathan Brodrick <jonathanbrodrick@gmail.com>
2025-06-27 23:44:51 +0100

    address nits

--- lineax/_operator.py
+++ lineax/_operator.py
@@ -588,11 +588,11 @@ class JacobianLinearOperator(AbstractLinearOperator):
            `jax.jacrev`. Otherwise, if not specified it will be chosen
            by default according to input and output shape.
         """
-        assert jac in [
-            None,
-            "fwd",
-            "bwd",
-        ], f"jac argument of JacobianLinearOperator must be fwd, bwd or None, got {jac}"
+        if jac not in [None, "fwd", "bwd"]:
+            raise ValueError(
+                "`jac` argument of `JacobianLinearOperator` should be either "
+                "`'fwd'`, `'bwd'`, or `None`."
+            )
         if not _has_aux:
             fn = NoneAux(fn)
         # Flush out any closed-over values, so that we can safely pass `self`
