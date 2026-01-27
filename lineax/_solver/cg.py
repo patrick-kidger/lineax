051343612146e9108bf05fe21c441ea83fe90b87
Jonathan Brodrick <jonathanbrodrick@gmail.com>
2026-01-27 11:02:58 +0000

    fix precommit

--- lineax/_solver/cg.py
+++ lineax/_solver/cg.py
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import warnings
 from collections.abc import Callable
 from typing import Any, TypeAlias
-import warnings
 
 import equinox.internal as eqxi
 import jax
